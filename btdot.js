c17e1ab03259766cf6504c2c561043573595bfe5
Barry (@nolim1t) <barry.teoh@perceptionz.net>
2011-08-02 13:53:28 +1000

    Update site for express 2.4.3 and heroku

---
+++ btdot.js
@@ -0,0 +1,8 @@
+#!/opt/node/bin/node
+
+require("sys").puts("Ok lets do this... Leeerrroy Jeeeeeenkins");
+var dwarf = require("./food"); // Dwarf Needs Food!
+var http = require('http');
+var port = process.env.PORT || 3000;
+app.listen(port);
+require('./routes');
--- food.js
+++ food.js
@@ -5,11 +5,8 @@ app.set('view engine', 'ejs');
 
 app.configure(function () {
     app.use(e.logger('\x1b[33m:method\x1b[0m \x1b[32m:url\x1b[0m (:response-time ms)'));
-    app.use(e.bodyDecoder());
-    app.use(e.methodOverride());
-    app.use(e.cookieDecoder());
     app.use(app.router);
-    app.use(e.staticProvider(__dirname + '/static'));
+    app.use(e.static(__dirname + '/static'));
 	app.set('views', __dirname + '/views');
     app.use(e.errorHandler({ dumpExceptions: false, showStack: false }));
 
