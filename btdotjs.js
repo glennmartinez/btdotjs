3502b595a109ca59a8aa6f3620ad4efda3dbbfe4
Barry (@nolim1t) <barry.teoh@perceptionz.net>
2011-08-02 13:54:27 +1000

    Remove btdotjs

--- btdotjs.js
+++
@@ -1,7 +0,0 @@
-#!/opt/node/bin/node
-
-require("sys").puts("Ok lets do this... Leeerrroy Jeeeeeenkins");
-var dwarf = require("./food"); // Dwarf Needs Food!
-var http = require('http');
-app.listen(8004);
-require('./routes');
