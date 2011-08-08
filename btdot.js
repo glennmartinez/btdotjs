6bda983bfdc646ba025f563fb4461ec038f70b20
Barry (@nolim1t) <barry.teoh@perceptionz.net>
2011-08-08 16:04:21 +1000

    Notify of the port number within foreman

--- btdot.js
+++ btdot.js
@@ -1,8 +1,9 @@
 #!/opt/node/bin/node
 
-require("sys").puts("Ok lets do this... Leeerrroy Jeeeeeenkins");
+
 var dwarf = require("./food"); // Dwarf Needs Food!
 var http = require('http');
 var port = process.env.PORT || 3000;
+require("sys").puts("Ok lets do this... Leeerrroy Jeeeeeenkins (Listening on port " + port + ")");
 app.listen(port);
 require('./routes');
